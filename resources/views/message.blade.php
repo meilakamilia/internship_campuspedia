bda7d9c4d7619c0b0e99744835d2c467c8b6094c
camiliamay6 <37019776+camiliamay6@users.noreply.github.com>
2019-12-01 19:01:23 +0700

    Update message.blade.php

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,3 +1,4 @@
+<!-- Fungsi alert untuk menampilkan pesan sukses dalam penambahan, pengeditan maupun penghapusan -->
 @if($message = Session::get('success'))
 <div class="alert alert-success alert-block">
     <button type="button" class="close" data-dismiss="alert"></button>
